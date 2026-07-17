94e0995c6dd7854fdd7beb7aff265653b2c5e656
barazaghost <kkeizzah11@gmail.com>
2026-07-17 09:14:48 +0300

    Add 'id' command to fetch Facebook User ID

--- fbot/Utility.js
+++ fbot/Utility.js
@@ -1,1 +1,31 @@
+const { keith } = require("../commandHandler");
 
+keith({
+    name: "id",
+    aliases: ["uid", "whois"],
+    category: "Utility",
+    usePrefix: false,
+    usage: "id [@mention]",
+    version: "1.0",
+    description: "Fetch the Facebook User ID (UID) of a mentioned user or yourself.",
+    admin: false,
+    cooldown: 10,
+
+    execute: async ({ client, event, reply, keithApi }) => {
+        const { threadID, messageID, senderID, mentions } = event;
+        
+        let uid;
+        let userName;
+
+        // Check if a user is mentioned
+        if (Object.keys(mentions).length > 0) {
+            uid = Object.keys(mentions)[0]; // Get the first mentioned user's ID
+            userName = mentions[uid].replace("@", ""); // Get mentioned user's name
+        } else {
+            uid = senderID; // If no mention, get the sender's UID
+            userName = "You";
+        }
+        
+        reply(`🔍 Facebook UID for ${userName}: ${uid}`);
+    }
+});
