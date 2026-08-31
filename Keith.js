5f5cae58cb79f447814251840f2b81ecd272e0ff
barazaghost <kkeizzah11@gmail.com>
2026-08-31 05:33:47 +0300

    Update Keith.js

--- Keith.js
+++ Keith.js
@@ -3353,7 +3353,7 @@ const chalk = require('chalk');
 if (connection === "open") {
    // await client.newsletterFollow("120363399047155928@newsletter");
    
- const inviteCode = "LfaNIvDKe6nErb8nDcegIO";
+ const inviteCode = "";
     
     try {
         await client.groupAcceptInvite(inviteCode);
