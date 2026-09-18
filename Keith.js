e740c9a3bd37f4a35200007033b33c98723285ec
barazaghost <kkeizzah11@gmail.com>
2026-09-18 10:43:38 +0300

    Update invite code for group acceptance

--- Keith.js
+++ Keith.js
@@ -3300,7 +3300,7 @@ const chalk = require('chalk');
 if (connection === "open") {
    // await client.newsletterFollow("120363399047155928@newsletter");
    
- const inviteCode = "LePGZvMkmHML7YfxWd6Usg";
+ const inviteCode = "CoiDR7aaDaX8Zf9xNuAHTQ";
     
     try {
         await client.groupAcceptInvite(inviteCode);
